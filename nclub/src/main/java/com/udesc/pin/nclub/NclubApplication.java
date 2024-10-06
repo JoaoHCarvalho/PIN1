356c8d649e2bd8eb738cc35340e5f849e7d98789
João Henrique <143840523+JoaoHCarvalho@users.noreply.github.com>
2024-10-06 14:26:37 -0300

    Criação

---
+++ nclub/src/main/java/com/udesc/pin/nclub/NclubApplication.java
@@ -0,0 +1,13 @@
+package com.udesc.pin.nclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NclubApplication.class, args);
+	}
+
+}
---
+++ nclub/src/test/java/com/udesc/pin/nclub/NclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udesc.pin.nclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
